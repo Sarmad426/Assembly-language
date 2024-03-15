15b7c80097ec3b23d9965615aada99a9f3f91a64
Muhammad Sarmad <sarmadrafique040@gmail.com>
2024-03-15 15:46:46 +0500

    Added hello world code

--- basic-program/first_program.asm
+++ basic-program/first_program.asm
@@ -1,18 +1,36 @@
-.model small
-.stack 100h
-.data
-str db "First Program!$"
-.code
+@ .model small
+@ .stack 100h
+@ .data
+@ str db "First Program!$"
+@ .code
 
-Main proc
-Move ax, @data
-Mov ds, ax
+@ Main proc
+@ Move ax, @data
+@ Mov ds, ax
 
-Lea Dx, str
-Mov ah, 9
-int 21h
+@ Lea Dx, str
+@ Mov ah, 9
+@ int 21h
 
-Mov ax, 4ch
-int 21h
-Main endp
-end Main
+@ Mov ax, 4ch
+@ int 21h
+@  Main endp
+; end Main
+
+section .data
+hello:     db 'First, Assembly program!',10    ; 'Hello, World!' plus a linefeed character
+helloLen:  equ $-hello             ; Length of the 'Hello world!' string
+
+section .text
+global _start
+
+_start:
+mov eax,4            ; The system call for write (sys_write)
+mov ebx,1            ; File descriptor 1 - standard output
+mov ecx,hello        ; Put the offset of hello in ecx
+mov edx,helloLen     ; helloLen is a constant, so we don't need to say
+                     ;  mov edx,[helloLen] to get it's actual value
+int 80h              ; Call the kernel
+mov eax,1            ; The system call for exit (sys_exit)
+mov ebx,0            ; Exit with return "code" of 0 (no error)
+int 80h;
